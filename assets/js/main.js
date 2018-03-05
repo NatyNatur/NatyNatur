47228c530303a469c653c64e8ce25ef85a80546c
NatyNatur <naticcg@gmail.com>
2018-03-05 00:23:39 -0300

    hiding content, changing sidenav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
+  $('#all').hide();
+  $("#btnEn").click(function(){
+    $("#particles-js").fadeOut();
+    $("#all").fadeIn("slow");
+    //$("#div3").fadeIn(3000);
+    });
   $(".button-collapse").sideNav();
   $('.carousel').carousel();
+
 })
 
 setInterval(function() {
